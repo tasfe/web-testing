0cc4a9f5c274543320effabc65330e3edc9a9b1e
hegyi.hajnalka92@gmail.com <hegyi.hajnalka92@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-03-21 11:28:05 +0000

    Proba commit.

--- web-testing/index.php
+++ web-testing/index.php
@@ -2,6 +2,12 @@
 echo "1. Ez egy teszt!<br/>\n";
 echo "2. Commit teszt.<br/>\n";
 echo "3. Masodik commit teszt.<br/>\n";
-phpinfo();
+// phpinfo();
+
+?>
+
+<input type="submit" name="a" value="OK" />
+
+<?php
 
 ?>
