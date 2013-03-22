175d2b476d8aa7dd86884348500e98f9f5cbbcf4
hjam1029@gmail.com <hjam1029@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-03-22 11:01:48 +0000

    branch1

---
+++ branches/branch1/web-testing/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "1. Ez egy teszt!<br/>\n";
+echo "2. Commit teszt.<br/>\n";
+echo "3. Masodik commit teszt.<br/>\n";
+// phpinfo();
+
+?>
+
+<input type="submit" name="a" value="OK" />
+
+<?php
+
+?>
