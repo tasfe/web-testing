f1c56accf2be77f5e01fbb36d3c46c71ee4f7f4d
szekelynoemi21@gmail.com <szekelynoemi21@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-03-25 08:54:44 +0000

    Noemi - elso commit

--- web-testing/index.php
+++ web-testing/index.php
@@ -3,7 +3,8 @@ echo "1. Ez egy teszt!<br/>\n";
 echo "2. Commit teszt.<br/>\n";
 echo "3. Masodik commit teszt.<br/>\n";
 echo "4. Sziasztok!<br/>\n";
-echo "5. proba <br/>\n"
+echo "5. proba <br/>\n";
+echo "6. Hahooo :) <br/>\n";
 // phpinfo();
 
 ?>
