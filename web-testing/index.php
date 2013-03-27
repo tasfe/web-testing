420c678f5e5d43fbba7ee68c72c81fecfb650a42
hadnagy.kinga9@gmail.com <hadnagy.kinga9@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-03-27 14:53:30 +0000

    Mai munkánk Renivel.

--- web-testing/index.php
+++ web-testing/index.php
@@ -1,16 +1,82 @@
 <?php
-echo "1. Ez egy teszt!<br/>\n";
-echo "2. Commit teszt.<br/>\n";
-echo "3. Masodik commit teszt.<br/>\n";
-echo "4. Sziasztok!<br/>\n";
-echo "5. proba <br/>\n";
-echo "6. Hahooo :) <br/>\n";
-// phpinfo();
 
 ?>
 
-<input type="submit" name="a" value="OK" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
-<?php
+<head>
+  <title>Tesztelo rendszer</title>  
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=9" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/image_slide.js"></script>
+</head>
 
-?>
+<body>
+  <div id="main">
+    <div id="header">
+	  <div id="banner">
+	    <div id="welcome">
+	      <h1><span>Tesztelo</span> rendszer</h1>
+	    </div><!--close welcome-->
+	    <div id="welcome_slogan">
+	      <h1>"Szeretnünk és ápolnunk kell a tévedést, mert o a megismerés anyaöle." (Nietzsche)</h1>
+	    </div><!--close welcome_slogan-->
+	  </div><!--close banner-->
+    </div><!--close header-->
+
+	<div id="menubar">
+      <ul id="menu">
+        <li class="current"><a href="index.html">Fooldal</a></li>
+        <li><a href="login.html">Bejelentkezés</a></li>
+      </ul>
+    </div><!--close menubar-->	
+    
+	<div id="site_content" align = "center">		
+	
+      <ul class="slideshow">
+        <li class="show"><img width="680" height="250" src="images/home_1.jpg" alt="&quot;Enter your caption here&quot;" /></li>
+        <li><img width="680" height="250" src="images/home_2.jpg" alt="&quot;Enter your caption here&quot;" /></li>
+      </ul>   	 
+	 
+	  <div id="content">
+        <div class="content_item" align = "left">
+
+			<br></br>
+		
+			<h1>Üdvözlünk az oldalunkon!</h1> 
+			<p>Az oldal rövid leírása itt található.</p>
+			
+			<div class="content_image" align = "left">
+				<img src="images/content_image1.jpg" alt="image1"/>
+			</div>
+			<p>Kép melletti szöveg itt található.</p>
+			<p>Újabb kép melletti szöveg bekezdés.</p>
+			<br style="clear:both"/>
+			
+			<br></br>
+			<br></br>
+			
+			<h1>Elérhetoség</h1>
+			<p>Telefonszám: +44 (0)1234 567891</p>
+			<p>Email: <a href="mailto:info@youremail.co.uk">info@youremail.co.uk</a></p>
+					  
+		</div><!--close content_item-->
+      </div><!--close content-->  
+	  
+	</div><!--close site_content--> 
+    
+	<div id="content_grey">  
+      <p>Hasonlo weboldalak</p>   
+	  <br style="clear:both"/>
+    </div><!--close content_grey-->  
+
+	<br></br>
+	<br></br>
+ 
+  </div><!--close main-->
+ 
+</body>
+</html>
