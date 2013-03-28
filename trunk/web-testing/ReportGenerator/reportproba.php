2b6e7010c0694125f0bcc78c4ad5626e9fcd86ae
hjam1029@gmail.com <hjam1029@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-03-28 13:45:53 +0000

    Reportok: Én és Karola

--- trunk/web-testing/ReportGenerator/reportproba.php
+++ trunk/web-testing/ReportGenerator/reportproba.php
@@ -4,6 +4,7 @@ $fname = 'Diploma.pdf';
 $pdf=new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',36);
+//Kép még nincs berakva
 //$pdf->Image(‘img/left1.png’, 10, 10, 35, 35);
 $pdf->SetTextColor(0, 255, 0);
 $pdf->Text(80, 100, 'Oklevél');
