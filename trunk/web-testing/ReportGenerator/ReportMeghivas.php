6d2d80ce2671d2f62f48c34a4bf919e0ebd9f5e0
hjam1029@gmail.com <hjam1029@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-03-28 13:40:42 +0000

    Reports

---
+++ trunk/web-testing/ReportGenerator/ReportMeghivas.php
@@ -0,0 +1,10 @@
+<?php 
+echo "Diploma letöltése: <br/>\n";
+		
+?>
+<form name="input"
+action="reportproba.php"
+method="post"
+>
+<input type="submit" value="Mentés">
+</form>
---
+++ trunk/web-testing/ReportGenerator/reportproba.php
@@ -0,0 +1,11 @@
+<?php
+require('pdfgen/fpdf.php');
+$fname = 'Diploma.pdf';
+$pdf=new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',36);
+//$pdf->Image(‘img/left1.png’, 10, 10, 35, 35);
+$pdf->SetTextColor(0, 255, 0);
+$pdf->Text(80, 100, 'Oklevél');
+$pdf->Output('Diploma.pdf', 'I');
+?>
