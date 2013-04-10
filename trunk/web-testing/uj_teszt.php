60bfaab0c2da5cee38e6f3e0bc7d6777fb432245
laszlorenata1991@gmail.com <laszlorenata1991@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-04-10 12:51:25 +0000

    lassuk nalatok hogy jeleniti meg az ekezetes karaktereket

--- trunk/web-testing/uj_teszt.php
+++ trunk/web-testing/uj_teszt.php
@@ -1,8 +1,10 @@
+<?php ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-">
   <title>Tesztelo rendszer</title>
   <meta http-equiv="X-UA-Compatible" content="IE=9" />
   <link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -17,10 +19,10 @@
     <div id="header">
 	  <div id="banner">
 	    <div id="welcome">
-	      <h1><span>Tesztelo</span> rendszer</h1>
+	      <h1><span>Tesztelõ</span> rendszer</h1>
 	    </div><!--close welcome-->
 	    <div id="welcome_slogan">
-	      <h1><span>"Szeretnunk es apolnunk kell a tevedest,</span> mert o a megismeres anyaole." (Nietzsche)</h1>
+	      <h1><span>"Szeretnünk es apolnunk kell a tevedest,</span> mert o a megismeres anyaole." (Nietzsche)</h1>
 	    </div><!--close welcome_slogan-->
 	  </div><!--close banner-->
     </div><!--close header-->
