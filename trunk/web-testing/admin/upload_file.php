bc66099e0817af6702cbe3d6c82f04a6f6988ed1
hegyi.hajnalka92@gmail.com <hegyi.hajnalka92@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-04-11 12:37:08 +0000

    Validalas megoldva feltoltes elott.

--- trunk/web-testing/admin/upload_file.php
+++ trunk/web-testing/admin/upload_file.php
@@ -1,21 +1,39 @@
 <?php
-	if ((($_FILES["file"]["type"] == "image/gif")
-			|| ($_FILES["file"]["type"] == "image/pjpeg"))) {
+
+function validate($file) {
+
+	$xml = new DOMDocument();
+	$xml->load($file);
+
+	chdir("../xml");
+	if (!$xml->schemaValidate("TesztSema.xsd")) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+	if ((($_FILES["file"]["type"] == "text/xml")
+			|| ($_FILES["file"]["type"] == "image/jpg"))) {
 		if ($_FILES["file"]["error"] > 0)
 		{
 			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
 		}
 		else
 		{
-			if (file_exists("../tests/" . $_FILES["file"]["name"]))
+			if (file_exists("../tests/" . $_FILES["file"]["tmp_name"]))
 			{
 				echo $_FILES["file"]["name"] . " already exists. ";
 			}
 			else
 			{
-				move_uploaded_file($_FILES["file"]["tmp_name"],
-				"../tests/" . $_FILES["file"]["name"]);
-				
+				echo $_FILES["file"]["tmp_name"];
+				if (validate($_FILES["file"]["tmp_name"])) {
+					move_uploaded_file($_FILES["file"]["tmp_name"],
+					"../tests/" . $_FILES["file"]["name"]);
+				} else
+					echo "Invalid file";
 			}
 		}
 	}
