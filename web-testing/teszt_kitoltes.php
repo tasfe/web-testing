1481bb48288427302f74227bc7a9c8d171e73a03
hegyi.hajnalka92@gmail.com <hegyi.hajnalka92@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-04-18 18:08:27 +0000

    Modositottam a feluletet, dinamikusan ir ki kerdeseket, modosit megfeleloen minden adatot, a pontszam meg nem mukodik, illetve utolso kerdes utani leallas.

--- web-testing/teszt_kitoltes.php
+++ web-testing/teszt_kitoltes.php
@@ -1,5 +1,4 @@
 <?php
-	$i = 0;
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -19,10 +18,14 @@
     <div id="header">
 	  <div id="banner">
 	    <div id="welcome">
-	      <h1>Teszt címe</h1>
+	      <h1> Cím: <?php
+	      		include 'readQuestion.php';
+	      		echo readName("tests/AlgebraelmH.xml"); ?>
+		</h1>
 	    </div><!--close title-->
 	    <div id="test_information">
-	      <h3>Megjegyzés: Az alábbi teszt kérdéseinek maximum ... helyes válasza lehet!</h3>
+	      <h3>Megjegyzés: Az alábbi teszt kérdéseinek maximum <?php
+	      		echo readCorrect("tests/AlgebraelmH.xml"); ?> helyes válasza lehet!</h3>
 	    </div><!--close information-->
 	  </div><!--close banner-->
     </div><!--close header-->
@@ -30,9 +33,14 @@
 	<div id="menubar">
 	<div id="menubar2">
       <ul id="menu">
-        <li>Tesztkérdések száma:...</li>
-        <li>Eddigi kérdések:...</li>
-        <li>Eddig elért pontszám:.../...</li>
+        <li>Tesztkérdések száma: <?php
+	      		echo readQnumber("tests/AlgebraelmH.xml"); ?></li>
+        <li>Eddigi kérdések: <?php
+                      if (!empty($_POST['q'])) echo $_POST['q']-1;
+                      else echo "0";
+                    ?></li>
+        <li>Eddig elért pontszám:<?php if (!empty($_POST['p'])) echo $_POST['p'];
+                      else echo "0";?>/<?php echo readPoints("tests/AlgebraelmH.xml");?></li>
       </ul>
       </div>
     </div><!--close menubar-->	
@@ -41,32 +49,84 @@
 	 
 	  <div id="content">
         <div class="content_item" align = "left">
-<?php
-	$i++;
-?>
-
-			<p><font size="6"><?php
-                      echo $i;
-                    ?>.Kérdés</font></p> <br>
+			<p><font size="6"><?php			
+                      if (!empty($_POST['q'])) {
+                      	$reply = readQ($_POST['q']-1, "tests/AlgebraelmH.xml");
+                      		if ($reply[0] == "ok")
+							 echo $_POST['q'] . ".";
+						}
+                      else {
+							$reply = readQ(0, "tests/AlgebraelmH.xml");
+							echo "1.";
+							} echo $reply[1];?> </font></p> <br>
 			
-			<form>
+			<form action="teszt_kitoltes.php" method="post">
 			<p><font size="5">
-			<input type="checkbox" name="valasz[]" value="valasz1"> 1Válasz<br><br>
-			<input type="checkbox" name="valasz[]" value="valasz2"> 2Válasz<br><br>
-			<input type="checkbox" name="valasz[]" value="valasz3"> 3Válasz<br><br>
-			<input type="checkbox" name="valasz[]" value="valasz4"> 4Válasz<br><br>
-			<input type="checkbox" name="valasz[]" value="valasz5"> 5Válasz<br><br>
+			
+			<?php 
+			$answers = 0;
+			foreach ($reply as $r)
+				$answers++;
+			
+			$j = 0;
+			for ($i=2; $i < $answers; $i++) {
+				if ($i % 2 == 0) {?>
+			<input type="checkbox" name="valasz[]" value="<?php echo $j; $j++; ?>"> <?php echo $reply[$i];?><br><br>
+			<?php } else {
+			}	
+			}
+			?>
+<!-- 			<input type="checkbox" name="valasz[]" value="valasz1"> 1Válasz<br><br> -->
+<!-- 			<input type="checkbox" name="valasz[]" value="valasz2"> 2Válasz<br><br> -->
+<!-- 			<input type="checkbox" name="valasz[]" value="valasz3"> 3Válasz<br><br> -->
+<!-- 			<input type="checkbox" name="valasz[]" value="valasz4"> 4Válasz<br><br> -->
+<!-- 			<input type="checkbox" name="valasz[]" value="valasz5"> 5Válasz<br><br> -->
 			<input type="submit" value="Válasz küldése"/>
-			</font></p>
-			</form>
-			<?php
-				if(!empty($_POST['valasz'])) {
- 				   foreach($_POST['valasz'] as $bejelolt) {
-            			echo $bejelolt;
-            		}
-            	}
+			<input type="hidden" name="q" value="
+			<?php 
+			
+			if (!empty($_POST['q'])) echo $_POST['q']+1;
+                      else echo "2";
             ?>
-          
+			"/>
+			<input type="hidden" name="p" value="
+			<?php
+			$pont = 0;
+			$helyes = 0;
+			$check = readOneCorrectPoint("tests/AlgebraelmH.xml");
+			
+			for ($i=3; $i < $answers; $i++) {
+				if (($i % 2 == 1) && ($reply[$i] == "true"))
+					$helyes++;
+			}
+			
+			if(!empty($_POST['valasz'])) {
+				foreach($_POST['valasz'] as $bejelolt) {
+ 					$index = 3 + 2*$bejelolt;
+					//echo $bejelolt;
+ 					if ($reply[$index] == "true") {
+						$pont++;	
+ 					}
+				}
+			} else echo "itt";
+			
+			if ($helyes == $pont) {
+				$pont = $pont*$check[0];
+			} else {
+				if ($check[1] == "igen")
+					$pont = $pont*$check[0];
+				else
+					$pont = 0;
+			}
+
+			if (!empty($_POST['p']))
+				echo $_POST['p'] + $pont;
+			else
+				echo $pont;
+            ?>
+			"/>
+			</font></p>
+			</form>          
 					  
 		</div><!--close content_item-->
 		
