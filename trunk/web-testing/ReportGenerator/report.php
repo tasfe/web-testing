df0ff399a63c851e8b89162bcaad436c535242ef
hjam1029@gmail.com <hjam1029@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-04-21 09:31:30 +0000

    Alternativ_generator_befejezve: minden működik. Kipróbálni a web-testing/reportProba.php - ben lehet

--- trunk/web-testing/ReportGenerator/report.php
+++ trunk/web-testing/ReportGenerator/report.php
@@ -11,16 +11,16 @@ function report($us, $ans) {
 	$this->user = $us;
 	$this->answers = $ans;
 }
-function irdKi($R=0, $G=0, $B=0) {
+function irdKi($user, $R=0, $G=0, $B=0) {
 		require('pdfgen/fpdf.php');
 		$this->pdf=new FPDF();
 		$this->pdf->AddPage();
 		
 		 $this->pdf->SetFont('Arial', '', 12);
 		$this->pdf->SetTextColor($R, $G, $B);
-	 	$this->pdf->write(5, "A felhasználó neve: ?"."\n");
-    	$this->pdf->write(5, "Felhasználó felhasználóneve: ?"."\n");
-	     $this->pdf->write(5, "Felhasználó emailcíme: ?"."\n\n\n\n");	
+	 	$this->pdf->write(5, "A felhasználó neve: ". $user[0]."\n");
+    	$this->pdf->write(5, "Felhasználó felhasználóneve: ".$user[1]."\n");
+	     $this->pdf->write(5, "Felhasználó emailcíme: ".$user[2]."\n\n\n\n");	
 	
 		$this->pdf->SetFont('Arial', 'B', 54);
 		$this->pdf->SetTextColor(0, 0, 0);
@@ -46,6 +46,16 @@ function ujKerdes($kerdes, $pt) {
 	$this->kerds++;
 	
 }	
+function envalaszaim($ans) {
+	$this->pdf->write(10, "Ezt(ezeket) válaszolta: \n");
+	$num = count($ans);
+	for($i = 0; $i < $num; ++$i) 
+		$this->pdf->write(5, $ans . ",\n");
+	$this->pdf->write(10, "\n");
+} 
+function pontokSzama($pontok) {
+	$this->pdf->write(10, "Az elért pontjaidnak a száma: " .$pontok);
+}
 function lezar() {
 	$this->pdf->Output('Diploma.PDF', 'I');
 }
--- trunk/web-testing/reportProba.php
+++ trunk/web-testing/reportProba.php
@@ -1,6 +1,49 @@
 <?php
 require("ReportGenerator/meghivas.php");
 require("readQuestion.php");
-$x = atalakitReportTombe('xml/Proba.xml');
-meghivas($x, 100, 20, 30);
+//function meghivas($kerdesek,$pont, $user, $answers, $pontok $R=0, $G=0, $B=0);
+ /* A meghivas parameterei:
+  * 
+  * $kerdesek --> readQuestion.php -ban lévõ atalakitReportTombe - függvény által
+  * visszatérített tömb; (kötelezõ megadni)
+  * pontszám --> readOneCorrectPoint($teszt) --> eredménye
+  * $user --> egy tömb aminek az elemei:
+  * Név, felhasználónév, emailcím(kötelezõ megadni)
+  * Az answers egy olyan tömb, aminek az elemei, a 
+  * felhasználó válaszai a kérdésre. Mûködik egyválasztos, és többválasztos
+  * kérdések esetén is. (kötelezõ megadni)
+  * $R, $G, $B - a szöveg rgb színe, nem kötelezõ megadni
+  * Példa: lennebb  
+  * $pontok --> az elért pontszámot tartalmazza
+  */
+
+// kérdések beálítása
+ $kerdesek = atalakitReportTombe('xml/Proba.xml');
+ // felhasználó beálítása
+ $felhasznalo = array();
+ $felhasznalo[0] = "Vak Béla";
+ $felhasznalo[1] = "saito";
+ $felhasznalo[2] = "vakbela@gmail.com";
+ // válaszok beálítása
+ $ans = array();
+ // egy helyes válasz van esetén:
+ $ans[0] = 'a';
+ $ans[1] = 'b';
+ // több helyes válasz van esetén:
+ /*
+  * $valasz1 = array();
+  * $valasz1[0] = 'a';
+  * $valasz1[1] = 'b';
+  * 
+  * $valasz2 = array();
+  * $valasz2[0] = 'a';
+  * $valasz2[1] = 'd';
+  *  $ans[0] = $valasz1;
+  *  $ans[1] = $valasz2;
+  */
+ // pontszám beálítása:
+  $pont = readOneCorrectPoint('xml/Proba.xml');
+  // elért pontok :
+  $pontok = 5;
+ meghivas($kerdesek,$pont, $felhasznalo, $ans, $pontok);
 ?>
