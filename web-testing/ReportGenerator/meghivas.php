ee4342484c70880ade0085ff8f6c98a8a3d5da1e
hjam1029@gmail.com <hjam1029@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-04-21 09:31:30 +0000

    Alternativ_generator_befejezve: minden működik. Kipróbálni a web-testing/reportProba.php - ben lehet

--- web-testing/ReportGenerator/meghivas.php
+++ web-testing/ReportGenerator/meghivas.php
@@ -1,15 +1,18 @@
 <?php
-function meghivas($kerdesek, $R=0, $G=0, $B=0) {
+function meghivas($kerdesek,$pont, $user, $answers, $pontok, $R=0, $G=0, $B=0) {
   require('report.php');
-  $generator = new report("valaki", "valami");
-  $generator->irdKi($R, $G, $B);
+  $generator = new report();
+  $generator->irdKi($user, $R, $G, $B);
   $num = count($kerdesek);
   for($i = 0; $i <$num; ++$i) {
  	$x = $kerdesek[$i];
- 	if($x[0] === 'ok')
+ 	if($x[0] === 'ok') {
  		$generator->ujKerdes($x, $pont[0]);
+ 		$generator->enValaszaim($answers[$i]);
+ 	}
  	
  }
+ $generator->pontokSzama($pontok);
 
 while (ob_get_level())
 	ob_end_clean();
