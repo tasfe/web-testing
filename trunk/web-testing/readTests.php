0836aa5b3b457048a8d4175755591a11a3f0bcf0
hegyi.hajnalka92@gmail.com <hegyi.hajnalka92@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-04-24 14:50:30 +0000

    Kiolvassa a teszt nevét ha a megfelelő kategóriából van.

---
+++ trunk/web-testing/readTests.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<?php
+
+function readTestName($kategoria) {
+	$dom = new DOMDocument();
+	$dom->load("../xml/feltoltott_tesztek.xml");
+	$teszt = $dom->getElementsByTagName('teszt');
+	
+	$i = 1;
+	
+	foreach ($teszt as $t) {
+		$temp = $t->getElementsByTagName('kategoria')
+		->item(0)
+		->nodeValue;
+		
+		if ($temp == $kategoria) {
+			$temp2 = $t->getElementsByTagName('cim')
+			->item(0)
+			->nodeValue;
+			$reply[$i] = $temp2;
+			
+			$i++;
+		}
+	}
+	if ($i > 1)
+		$reply[0] = "ok";
+	else
+		$reply[0] = "hiba";
+					
+
+	return $reply;
+}
+
+?>
+
+</body>
+</html>
