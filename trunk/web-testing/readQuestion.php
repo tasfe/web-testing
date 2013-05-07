69d83a3d5af7359bb8c4fb0adee4575999614af3
hegyi.hajnalka92@gmail.com <hegyi.hajnalka92@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-05-07 16:01:04 +0000

    Frissítve egy kategória lekérdezéssel.

--- trunk/web-testing/readQuestion.php
+++ trunk/web-testing/readQuestion.php
@@ -10,37 +10,37 @@
 		$dom = new DOMDocument();
 		$dom->load($teszt);
 		$tt = $dom->getElementsByTagName('teszt');
-		
+
 		foreach ($tt as $t) {
 			$string = $t->getElementsByTagName('cim')
 			->item(0)
 			->nodeValue;
 		}
-		
+
 		return $string;
 	}
-	
+
 	//helyes valaszok maximalis szama
 	function readCorrect($teszt) {
 		$dom = new DOMDocument();
 		$dom->load($teszt);
 		$jellemzok = $dom->getElementsByTagName('jellemzok');
-	
+
 		foreach ($jellemzok as $j) {
 			$string = $j->getElementsByTagName('helyesvalaszszam')
 			->item(0)
 			->nodeValue;
 		}
-		
+
 		return $string;
 	}
-	
+
 	//hany helyes valasz van a tesztben
 	function readTrues($teszt) {
 		$dom = new DOMDocument();
 		$dom->load($teszt);
 		$feladat = $dom->getElementsByTagName('feladat');
-		
+
 		$helyesek = 0;
 		foreach ($feladat as $f)
 		{
@@ -62,73 +62,73 @@
 		}
 		return $helyesek;
 	}
-	
+
 	//osszegyujtheto pontok szama
 	function readPoints($teszt) {
 		$dom = new DOMDocument();
 		$dom->load($teszt);
 		$tt = $dom->getElementsByTagName('teszt');
-		
+
 		$kerdesek_szama = readQnumber($teszt);
-		
+
 		foreach ($tt as $t) {
 			$helyes_pont = $t->getElementsByTagName('egyhelyesvalaszp')
 			->item(0)
 			->nodeValue;
-		
+
 			$reszpont = $t->getElementsByTagName('reszpont')
 			->item(0)
 			->nodeValue;
-		
+
 			$kerdesek_szam = readQnumber($teszt);
 			$helyesek = readTrues($teszt);
-			
+				
 			if ($reszpont == "igen") {
 				$osszpontszam = $helyes_pont*$helyesek;
 			} else {
 				$osszpontszam = $helyes_pont*$kerdesek_szama;
 			}
 		}
-		
+
 		return $osszpontszam;
 	}
-	
+
 	//egy helyes valasznak a pontszama
 	function readOneCorrectPoint($teszt) {
 		$dom = new DOMDocument();
 		$dom->load($teszt);
 		$tt = $dom->getElementsByTagName('teszt');
-		
+
 		foreach ($tt as $t) {
 			$helyes_pont = $t->getElementsByTagName('egyhelyesvalaszp')
 			->item(0)
 			->nodeValue;
 			$response[0] = $helyes_pont;
-			
+				
 			$reszpont = $t->getElementsByTagName('reszpont')
 			->item(0)
 			->nodeValue;
 			$response[1] = $reszpont;
 		}
-		
+
 		return $response;
 	}
-	
+
 	//kerdesek szama
 	function readQnumber($teszt) {
 		$dom = new DOMDocument();
 		$dom->load($teszt);
 		$feladat = $dom->getElementsByTagName('feladat');
-		
+
 		$kerdesek_szama=0;
 		foreach ($feladat as $f)
 		{
 			$kerdesek_szama++;
 		}
-		
+
 		return $kerdesek_szama;
 	}
-	
+
 	//az i. kerdes es valaszai
 	function readQ($i, $teszt) {
 		$dom = new DOMDocument();
@@ -150,49 +150,65 @@
 							$reply[$j]=$string;
 							$j++;
 						} if ($subNodes->nodeName=="helyes")
-						  {
+						{
 							//helyesseg
 							$string = $subNodes->nodeValue;
 							$reply[$j]=$string;
 							$j++;
-							}
-						}
-						}
-						if ($nodename->nodeName=="kerdes")
-						{
-							//kerdes
-							$string = $nodename->nodeValue;
-							$reply[0]="ok";
-							$reply[1]=$string;
 						}
 					}
-				} else {
-						$reply[0]="hiba";
 				}
-	
-	return $reply;
-}
-// az összes kérdés egybokorban
-function atalakitReportTombe($teszt) {
-	$num = readQnumber($teszt);
-	$tomb = array();
-	for($i = 0; $i < $num; ++$i)
-		$tomb[$i] = readQ($i, $teszt);
-	return $tomb;
-}
+				if ($nodename->nodeName=="kerdes")
+				{
+					//kerdes
+					$string = $nodename->nodeValue;
+					$reply[0]="ok";
+					$reply[1]=$string;
+				}
+			}
+		} else {
+			$reply[0]="hiba";
+		}
 
-//echo readName("Proba.xml");
+		return $reply;
+	}
 
-//echo readCorrect("Proba.xml");
+	function readCategory($teszt) {
+		$dom = new DOMDocument();
+		$dom->load($teszt);
+		$jellemzok = $dom->getElementsByTagName('jellemzok');
+		
+		foreach ($jellemzok as $j) {
+			$string = $j->getElementsByTagName('kategoria')
+			->item(0)
+			->nodeValue;
+		}
+		
+		return $string;
+	}
 
-//echo readQnumber("Proba.xml");
 
-//echo readPoints("Proba.xml");
+	// az ï¿½sszes kï¿½rdï¿½s egybokorban
+	function atalakitReportTombe($teszt) {
+		$num = readQnumber($teszt);
+		$tomb = array();
+		for($i = 0; $i < $num; ++$i)
+			$tomb[$i] = readQ($i, $teszt);
+		return $tomb;
+	}
 
-// $rep = readQ(1, "Proba.xml");
-// foreach($rep as $r)
-// 	echo $r . " ";
-?>
+	//echo readName("Proba.xml");
+
+	//echo readCorrect("Proba.xml");
+
+	//echo readQnumber("Proba.xml");
+
+	//echo readPoints("Proba.xml");
+
+	// $rep = readQ(1, "Proba.xml");
+	// foreach($rep as $r)
+		// 	echo $r . " ";
+	?>
 
 </body>
 </html>
