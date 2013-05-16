4dfe84da5e85bd51c8e4bda514178c1d6195529c
szekelynoemi21@gmail.com <szekelynoemi21@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-05-16 10:54:42 +0000

    Kitoltott tesztek megjelenitese es lehetoseg report generalasra

--- trunk/web-testing/user/kitoltott_tesztek.php
+++ trunk/web-testing/user/kitoltott_tesztek.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include '../readQuestion.php';
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
@@ -66,17 +71,9 @@
 					<!--close sidebar-->
 					<div class="sidebar">
 						<div class="sidebar_item">
-							<h2>Kitöltött tesztek</h2>
+							<h2>Eddigi eredmények</h2>
 							<p>A fenti menüpont alatt megnézheted, illetve letöltheted a már
 								kitöltött tesztjeid.</p>
-						</div>
-						<!--close sidebar_item-->
-					</div>
-					<!--close sidebar-->
-					<div class="sidebar">
-						<div class="sidebar_item">
-							<a href="eredmenyek.php"><h2>Eredmények</h2> </a>
-							<p>A fenti menüpont alatt megnézheted az eddig elért eredmenyeid</p>
 						</div>
 						<!--close sidebar_item-->
 					</div>
@@ -101,14 +98,60 @@
 
 				<br></br>
 				<h2 align="center">Kitöltött tesztek</h2>
-				<p>Itt megtekintheted az eddig kitöltött teszteket.</p>
-				<p>Válassz egy témát a felsoroltak közül:</p>
-				<ul>
-					<li>Algebra</li>
-					<li>Geometria</li>
-					<li>Trigonometria</li>
-				</ul>
+				<p>Itt megtekintheted az eddig kitöltött teszteket és az elért eredményeket.</p>
+				
+				<?php
+				$db = 'adatok';
+				$host = 'localhost';
+				$user = 'root';
+				$pass = '';
+				
+				//connection to the database
+				$dbhandle = mysql_connect($host, $user, $pass)
+				or die("Nem lehet kapcsolódni MySQL-hez!");
+				
+				//select a database to work with
+				$selected = mysql_select_db($db)
+				or die("Nem sikerült kapcsolódni az adatbázishoz!");
 
+				$sql="SELECT emailcim, idTesztek, Datum, Eredmeny FROM kitoltotttesztek WHERE emailcim = '" . $_SESSION['your_email'] . "'";
+				$result=mysql_query($sql);
+				if(!$result)
+					echo "Még nincs kitöltött teszted!";//die("Sikertelen lekérdezés!");
+else {
+				if (isset($_SESSION['activation_result']))
+				{
+					echo '<br />';
+					echo '<b>'.$_SESSION['activation_result'].'</b>';
+					echo '<br />';
+					unset($_SESSION['activation_result']);
+				}
+							
+				echo '<form action = "activate.php" method = "post">';
+				echo '<TABLE BORDER="1" CELLPADDING="4" CELLSPACING="2">';
+				echo '<table><th>&emsp;</th><th>Tesztnév</th><th>Tesztre kapott jegy</th><th>Kitöltés dátuma</th>';
+				while ($row = mysql_fetch_assoc($result)) {
+					$sql2 = "SELECT TesztNev FROM tesztek WHERE idTesztek='" . $row['idTesztek'] . "'";
+					$result2=mysql_query($sql2);
+					$row2 = mysql_fetch_assoc($result2);
+					$cim = readName("../tests/" . $row2['TesztNev']);
+					echo '<tr>
+					<td><input type=\'radio\' name=\'radio\' VALUE='.$row2['TesztNev'].'"></td>
+					<td> '.$cim.' </td>
+					<td>'.$row['Eredmeny'].'</td>
+					<td>'.$row['Datum'].'</td>';
+				}
+				echo '</TABLE><br /><br />';
+				echo '<input type="submit" name="submit" value="Report generálása" />&emsp;';
+				echo '<br />';
+				
+				//free the resources associated with the result set
+				mysql_free_result($result);
+				//close connection
+				mysql_close($dbhandle);
+}
+			?>
+			<br><br>
 			</div>
 			<!--close content_item-->
 		</div>
