e9119dc35a9c4d3c9747a81df6d05b0401ee11a1
hegyi.hajnalka92@gmail.com <hegyi.hajnalka92@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-05-22 15:32:47 +0000

    kijelentkezeskor cookie torlese

--- trunk/web-testing/user/logout.php
+++ trunk/web-testing/user/logout.php
@@ -2,6 +2,14 @@
 <?php 
 // Put this code in first line of web page.
 session_start();
+$var = $_SESSION['your_email'];
 session_destroy();
-header("location:../login.php");
+?>
+<script type="text/javascript" src="../cookie.js"></script>
+<script>
+eraseCookie('<?php echo $var;?>'); window.location.href = '../login.php';
+</script>
+<?php
+
+//header("location:../login.php");
 ?>
