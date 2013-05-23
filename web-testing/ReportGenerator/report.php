d2d1c7a34ee11b579fbd6713aaef66c05c4ca2c5
hjam1029@gmail.com <hjam1029@gmail.com@04bd9059-e59c-9299-6d8d-2823dcbe9463>
2013-05-23 01:32:47 +0000

    ezzel már működnek a magyar karakterek

--- web-testing/ReportGenerator/report.php
+++ web-testing/ReportGenerator/report.php
@@ -1,4 +1,7 @@
 <?php
+header("Content-Type: text/html; charset=utf-8");
+?>
+<?php
 class report
 {
 var $user;
@@ -8,60 +11,54 @@ var $kerds = 1;
 /*************************************************/
 
 function report() {
-	
-}
 
+}
 function irdKi($user, $R = 0, $G = 0, $B = 0) {
-		require('pdfgen/fpdf.php');
-		$this->pdf=new FPDF();
+		require('tcpdf/tcpdf.php');
+		$this->pdf=new TCPDF();
 		$this->pdf->AddPage();
 		
-		 $this->pdf->SetFont('arial', '', 12);
+		 $this->pdf->SetFontSize(12);
 		$this->pdf->SetTextColor($R, $G, $B);
-	 	$this->pdf->write(5, "A felhasznalo neve: ". $user[0]."\n");
-	     $this->pdf->write(5, "Felhasznalo emailcime: ".$user[2]."\n\n\n\n");	
+	 	$this->pdf->write(5, "A felhasználó neve: ". $user[0]."\n");
+	     $this->pdf->write(5, "Felhasználó emailcíme: ".$user[2]."\n\n\n\n");	
 	
-		$this->pdf->SetFont('Arial', 'B', 54);
+		$this->pdf->SetFontSize(54);
 		$this->pdf->SetTextColor(0, 0, 0);
-		$this->pdf->write(50, '           Oklevel'."\n");
+		$this->pdf->write(50, '           Oklevél'."\n");
 	
-		$this->pdf->SetFont('Arial', '', 12);
+		$this->pdf->SetFontSize(12);
 		$this->pdf->SetTextColor($R, $G, $B);
 	}
-function szamoldHelyes($pt, $kerdes){
-	$length = count ($kerdes);
-	$db = 0;
-	for ($i = 0; $i< $length; ++$i)
-		$db += ($kerdes[$i] === 'true') ? $pt:0;
-	return $db;
-}
 function ujKerdes($kerdes, $pt) {
 	$leng = count($kerdes);
-	$pt = $this->szamoldHelyes ($pt, $kerdes);
 	$this->pdf->write(5, $this->kerds . ") ".$kerdes[1]. "   (".$pt . "pont)"."\n\n");
 	$i = 2;
 	$betu = 'a';
+	$helyesvalasz = "";
 	while($i < $leng) {
 		if($kerdes[$i+1] === 'true')
-			$this->pdf->SetFont('Arial','U', 12);
+			$helyesvalasz = $helyesvalasz . "     " . $kerdes[$i];
 		$this->pdf->write(5, " ".$betu.") ".$kerdes[$i] ."\n");
 		$i = $i + 2;
 		$betu++;
-		$this->pdf->SetFont('Arial','', 12);
+		$this->pdf->SetFontSize(12);
 	}
-	$this->pdf->write(5, "\n");
+	$this->pdf->write(5, "A helyes válasz(ok): \n" . "  " .$helyesvalasz);
+	$this->pdf->write(5, "");
 	$this->kerds++;
 	
 }	
 function envalaszaim($ans) {
-	$this->pdf->write(3, "Felhasznalo valaszai: ");
+	$this->pdf->write(10, "\nA felhasználó ezt(ezeket) választotta: \n");
 	$num = count($ans);
+	$this->pdf->write(5, "    ");
 	for($i = 0; $i < $num; ++$i) 
-		$this->pdf->write(3, $ans[$i] . "   ");
-	$this->pdf->write(10, "\n");
+		$this->pdf->write(5, $ans[$i] . ",    ");
+	$this->pdf->write(10, "\n\n\n\n");
 } 
 function pontokSzama($pontok) {
-	$this->pdf->write(10, "Az elert jegy: " .$pontok);
+	$this->pdf->write(10, "Az elért jegy: " .$pontok);
 }
 function lezar() {
 	$this->pdf->Output('Diploma.PDF', 'I');
